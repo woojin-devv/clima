f038603c7c1cb2d1f8d316a90f891fe68669c704
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-11-03 12:47:38 +0900

    Section140. - Android 관련 Permission 추가 - ios 관련 Permission 추가 - getLocation() 함수 정의

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,13 +9,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
+          onPressed: () async {
             //Get the current location
+            Position position = await Geolocator.getCurrentPosition(
+              locationSettings: LocationSettings(
+                accuracy: LocationAccuracy.high,
+              ),
+            );
+
+            print(position);
           },
           child: Text('Get Location'),
         ),
